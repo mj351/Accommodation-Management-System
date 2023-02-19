27d5d5231f96be0b5d49b312d56766068e731178
Marwan Jemal <mj351@canterbury.ac.uk>
2023-02-19 08:52:04 +0000

    connected to MongoDB Atlas

--- server/app.js
+++ server/app.js
@@ -3,21 +3,26 @@ const app = express()
 const port = 8000 
 const mongoose = require("mongoose");
 
-const dbUrl = "mongodb+srv://marwan:xxq6ilScsLlMM8cZ@cluster0.avzd0kp.mongodb.net/accommodation-app?retryWrites=true&w=majority"
+// Database
+const Database = (module.exports = () => { 
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  mongoose.set('strictQuery', false);
+  try {
+    mongoose.connect(
+      "mongodb+srv://marwan:xxq6ilScsLlMM8cZ@cluster0.avzd0kp.mongodb.net/accommodation?retryWrites=true&w=majority",
+      connectionParams
+      );
+      console.log("Successfully connected to MongoDB");
+    } catch(error) {
+      console.log(error);
+      console.log("Error connecting to MongoDB");
+  }
+});
 
-const  connectionParams = {
-  uesrNewUrlParser: true,
-  useUnifiedTopology: true
-}
-
-mongoose
-  .connect(dbUrl, connectionParams)
-  .then(() => {
-    console.info("Successfully connected to MongoDB");
-  })
-  .catch((e) => {
-    console.log("Error connecting to MongoDB:", err);
-  });
+Database();
 
 
 // Serve - https://stackoverflow.com/questions/4840879/nodejs-how-to-get-the-servers-port
