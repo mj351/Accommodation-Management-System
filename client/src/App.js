5deb78af2a643acf763b5f09f2aa4ff84280c91a
Marwan Jemal <mj351@canterbury.ac.uk>
2023-04-02 17:08:28 +0000

    test api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import AddStudentForm from './components/AddStudentForm';
+import StudentList from './components/StudentList';
+
 
 function App() {
   return (
     <div className="App">
       <main>
         <AddStudentForm />
+        <StudentList />
       </main>
     </div>
   );
