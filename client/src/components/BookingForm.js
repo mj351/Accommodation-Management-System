90f829c74474d7d0082242bbaaa5d5c6df9af89d
mj351 <80540936+mj351@users.noreply.github.com>
2023-05-07 21:07:51 +0100

    update booking form and booking management page

--- client/src/components/BookingForm.js
+++ client/src/components/BookingForm.js
@@ -1,96 +1,321 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_BASE_URL } from '../config';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../config";
+import Select from "react-select";
+import { AiOutlineUser } from "react-icons/ai";
+import { FaBed } from "react-icons/fa";
+import { Card, Col, Row, Button, Modal, Spinner } from "react-bootstrap";
 
-const BookingForm = ({ userId, students, rooms, onSelect }) => {
-  const [studentId, setStudentId] = useState('');
-  const [roomId, setRoomId] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  
+const BookingForm = ({ userId, students, rooms, onSelect, setRooms }) => {
+  const [student, setStudent] = useState(null);
+  const [room, setRoom] = useState(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [error, setError] = useState(null);
+  const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [modal, setModal] = useState(null);
+
+  // // console.log(startDate);
+  // // console.log(endDate);
+
+  useEffect(() => {
+    axios
+      .get(`${API_BASE_URL}/api/bookings`)
+      .then((response) => {
+        // // console.log(response.data);
+        setLoading(false);
+        setBookings(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post(`${API_BASE_URL}/booking`, {
-        userId,
-        studentId,
-        roomId,
+      setLoading(true);
+      // // console.log(student.value, room.value, startDate, endDate);
+      const response = await axios.post(`${API_BASE_URL}/api/bookings`, {
+        // userId,
+        studentId: student.value,
+        roomId: room.value,
         startDate,
         endDate,
       });
-
-      console.log('Booking created:', response.data);
+      // // console.log("Booking created:", response.data);
+      setBookings([response.data, ...bookings]);
+      setLoading(false);
+      setModal("Booking created successfully");
     } catch (error) {
-      console.error('Error creating booking:', error);
+      console.error("Error creating booking:", error.response.data.msg);
+      setError(error.response.data.msg);
+      setLoading(false);
     }
   };
 
+  const studentOptions = students.map((student) => ({
+    value: student._id,
+    label: `${student.firstName} ${student.lastName} (${student.studentId})`,
+  }));
+
+  const roomOptions = rooms.map((room) => ({
+    value: room._id,
+    label: `Room no: ${room?.roomNumber}`,
+  }));
+
   const handleSelect = () => {
     if (onSelect) {
-      onSelect({ studentId, roomId });
+      onSelect({ studentId: student.value, roomId: room.value });
     }
   };
 
   useEffect(() => {
     handleSelect();
-  }, [studentId, roomId]);
+  }, [student, room]);
+  // console.log(bookings);
 
+  const handleStartDateChange = (event) => {
+    const selectedStartDate = new Date(event.target.value);
+    let now = new Date();
+    now.setHours(0, 0, 0);
+    if (selectedStartDate <= now) {
+      setError("Start date should be greater than current date");
+      return;
+    }
+    if (endDate && selectedStartDate > new Date(endDate)) {
+      setError("Start date should be less than end date");
+      return;
+    }
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    const selectedEndDate = new Date(event.target.value);
+    const selectedStartDate = new Date(startDate);
+    if (selectedEndDate < selectedStartDate) {
+      setError("End date should be greater than start date");
+      return;
+    }
+    setEndDate(event.target.value);
+  };
+
+  const handleClose = () => {
+    setError(null);
+  };
+  const handleCloseModal = () => {
+    setModal(null);
+  };
+
+  if (loading) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* Existing form fields */}
-      <div>
-        <label htmlFor="studentId">Student</label>
-        <select
-          id="studentId"
-          value={studentId}
-          onChange={(event) => setStudentId(event.target.value)}
-        >
-          <option value="">Select Student</option>
-          {students.map((student) => (
-            <option key={student._id} value={student._id}>
-              {student.name} ({student.email})
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="roomId">Room</label>
-        <select
-          id="roomId"
-          value={roomId}
-          onChange={(event) => setRoomId(event.target.value)}
-        >
-          <option value="">Select Room</option>
-          {rooms.map((room) => (
-            <option key={room._id} value={room._id}>
-              {room.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="startDate">Start Date</label>
-        <input
-          type="date"
-          id="startDate"
-          value={startDate}
-          onChange={(event) => setStartDate(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="endDate">End Date</label>
-        <input
-          type="date"
-          id="endDate"
-          value={endDate}
-          onChange={(event) => setEndDate(event.target.value)}
-        />
-      </div>
-      <button type="submit">Book Room</button>
-    </form>
+    <>
+      <Row>
+        <Col lg={6} sm={12} className="mb-5">
+          <Card
+            text="light"
+            className="mb-2 shadow-bottom-right"
+            id="student-card"
+          >
+            <Card.Body
+              style={{
+                padding: "20px 50px 0 50px",
+              }}
+            >
+              <form onSubmit={handleSubmit} className="mt-3">
+                <div className="form-group">
+                  <Select
+                    id="studentId"
+                    classNamePrefix="react-select"
+                    value={student}
+                    onChange={setStudent}
+                    options={studentOptions}
+                    placeholder={
+                      <div>
+                        <AiOutlineUser style={{ color: "black" }} />
+                        <span style={{ marginLeft: "10px", color: "black" }}>
+                          Select Student
+                        </span>
+                      </div>
+                    }
+                    styles={{
+                      control: (provided) => ({
+                        ...provided,
+                        minHeight: "40px",
+                        borderColor: "#ced4da",
+                        "&:hover": { borderColor: "#80bdff" },
+                      }),
+                      placeholder: (provided) => ({
+                        ...provided,
+                        color: "black",
+                      }),
+                      singleValue: (provided) => ({
+                        ...provided,
+                        color: "black",
+                      }),
+                      option: (provided, state) => ({
+                        ...provided,
+                        backgroundColor: state.isFocused ? "#80bdff" : "white",
+                        color: state.isFocused ? "white" : "black",
+                      }),
+                    }}
+                    isSearchable={false}
+                  />
+                </div>
+                <div className="form-group">
+                  <Select
+                    id="roomId"
+                    classNamePrefix="react-select"
+                    value={room}
+                    onChange={setRoom}
+                    options={roomOptions}
+                    placeholder={
+                      <div>
+                        <FaBed style={{ color: "black" }} />
+                        <span style={{ marginLeft: "10px", color: "black" }}>
+                          Select Room
+                        </span>
+                      </div>
+                    }
+                    styles={{
+                      control: (provided) => ({
+                        ...provided,
+                        minHeight: "40px",
+                        borderColor: "#ced4da",
+                        "&:hover": { borderColor: "#80bdff" },
+                      }),
+                      placeholder: (provided) => ({
+                        ...provided,
+                        color: "black",
+                      }),
+                      singleValue: (provided) => ({
+                        ...provided,
+                        color: "black",
+                      }),
+                      option: (provided, state) => ({
+                        ...provided,
+                        backgroundColor: state.isFocused ? "#80bdff" : "white",
+                        color: state.isFocused ? "white" : "black",
+                      }),
+                    }}
+                    isSearchable={false}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="startDate">Check-IN Date</label>
+                  <input
+                    id="startDate"
+                    value={startDate}
+                    onChange={handleStartDateChange}
+                    type="date"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="endDate">Check-OUT Date</label>
+                  <input
+                    id="endDate"
+                    value={endDate}
+                    onChange={handleEndDateChange}
+                    type="date"
+                  />
+                </div>
+
+                <Modal show={!!error} onHide={handleClose} variant="danger">
+                  <Modal.Header closeButton>
+                    <Modal.Title>Error</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>{error}</Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                      Close
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+
+                <Modal
+                  show={!!modal}
+                  onHide={handleCloseModal}
+                  variant="danger"
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>Success</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>{modal}</Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="success" onClick={handleCloseModal}>
+                      Close
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+                <button
+                  type="submit"
+                  className="d-block mx-auto btn-booking"
+                  disabled={!student || !startDate || !endDate || !room}
+                >
+                  Book Room
+                </button>
+              </form>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col lg={6} sm={12}>
+          <Card
+            border="secondary"
+            // style={{ height: "460px", overflow: "auto" }}
+            className="shadow-bottom-right"
+          >
+            <Card.Header className="h3">Bookings</Card.Header>
+            <Card.Body style={{ height: "407px", overflow: "auto" }}>
+              {bookings.length === 0 && (
+                <Card
+                  border="secondary"
+                  style={{ width: "95%", margin: "0 auto 30px auto" }}
+                >
+                  <Card.Body>
+                    <Card.Title>No Booking Found</Card.Title>
+                  </Card.Body>
+                </Card>
+              )}
+              {bookings.map((booking) => (
+                <Card
+                  border="secondary"
+                  style={{ width: "95%", margin: "0 auto 30px auto" }}
+                  key={booking._id}
+                >
+                  <Card.Header
+                    className={
+                      booking.status === "cancelled" && "bg-danger text-light"
+                    }
+                  >
+                    Room No: {booking.room?.roomNumber}{" "}
+                    {booking.status === "cancelled" && "(cancelled)"}
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      {booking.student.firstName} {booking.student.lastName}
+                    </Card.Title>
+                    <Card.Text>
+                      Check-in Date:{" "}
+                      {new Date(booking.startDate).toDateString()}
+                    </Card.Text>
+                    <Card.Text>
+                      Check-out Date: {new Date(booking.endDate).toDateString()}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              ))}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
--- client/src/pages/BookingManagementPage.js
+++ client/src/pages/BookingManagementPage.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_BASE_URL } from '../config';
-import BookingForm from '../components/BookingForm';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../config";
+import BookingForm from "../components/BookingForm";
+import { Container } from "react-bootstrap";
 
 export const BookingManagementPage = () => {
   const [students, setStudents] = useState([]);
@@ -17,7 +18,7 @@ export const BookingManagementPage = () => {
       const response = await axios.get(`${API_BASE_URL}/api/students`);
       setStudents(response.data);
     } catch (error) {
-      console.error('Error fetching students:', error);
+      console.error("Error fetching students:", error);
     }
   };
 
@@ -26,24 +27,14 @@ export const BookingManagementPage = () => {
       const response = await axios.get(`${API_BASE_URL}/api/rooms`);
       setRooms(response.data);
     } catch (error) {
-      console.error('Error fetching rooms:', error);
-    }
-  };
-
-  const handleFormSubmit = async (bookingData) => {
-    try {
-      await axios.post(`${API_BASE_URL}/api/bookings`, bookingData);
-      alert('Booking created successfully!');
-    } catch (error) {
-      console.error('Error creating booking:', error);
-      alert('Failed to create booking.');
+      console.error("Error fetching rooms:", error);
     }
   };
 
   return (
-    <div className="booking-management">
-      <h1>Booking Management</h1>
-      <BookingForm onSubmit={handleFormSubmit} students={students} rooms={rooms} />
-    </div>
+    <Container className="booking-management">
+      <h1 className="text-center m-3">Booking Management</h1>
+      <BookingForm students={students} rooms={rooms} setRooms={setRooms} />
+    </Container>
   );
 };
