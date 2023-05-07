0f7303319f7d9980d3ce361c5bd862de924f274c
mj351 <80540936+mj351@users.noreply.github.com>
2023-05-07 20:45:42 +0100

    add css to room management page

--- client/src/components/AddRoomForm.js
+++ client/src/components/AddRoomForm.js
@@ -1,58 +1,148 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { API_BASE_URL } from '../config';
+import React, { useState } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../config";
+import { Card, Col, Row, Spinner, Modal, Button } from "react-bootstrap";
+import RoomList from "./RoomList";
+import { useEffect } from "react";
 
 const AddRoomForm = () => {
-  const [roomNumber, setRoomNumber] = useState('');
-  const [capacity, setCapacity] = useState('');
-  const [type, setType] = useState('');
-  const [description, setDescription] = useState('');
+  const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [modal, setModal] = useState(null);
+  const [roomNumber, setRoomNumber] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
   const [currentbookings, setCurrentBookings] = useState([]);
-  
+  const fetchRooms = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/api/rooms`);
+      setRooms(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching rooms:", error);
+    }
+  };
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+  const handleClose = () => {
+    setModal(null);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setLoading(true);
     try {
-      await axios.post(`${API_BASE_URL}/api/rooms`, { roomNumber, capacity, type, description, currentbookings });
-      alert('Room added successfully');
-      setRoomNumber('');
-      setCapacity('');
-      setType('');
-      setDescription('');
+      const { data } = await axios.post(`${API_BASE_URL}/api/rooms`, {
+        roomNumber,
+        capacity,
+        type,
+        description,
+        currentbookings,
+      });
+      setModal("Room added successfully");
+      setRooms([...rooms, data]);
+      setRoomNumber("");
+      setCapacity("");
+      setType("");
+      setDescription("");
       setCurrentBookings([]);
+      setLoading(false);
     } catch (error) {
-      console.error('Error adding room:', error);
-      alert('Error adding room');
+      console.error("Error adding room:", error);
+      setLoading(false);
+      alert("Error adding room");
     }
   };
 
   return (
-    <div>
-      <h2>Add Room</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Room Number:
-          <input type="text" value={roomNumber} onChange={(e) => setRoomNumber(e.target.value)} required />
-        </label>
-        <br />
-        <label>
-          Capacity:
-          <input type="number" value={capacity} onChange={(e) => setCapacity(e.target.value)} required />
-        </label>
-        <br />
-        <label>
-          Type:
-          <input type="text" value={type} onChange={(e) => setType(e.target.value)} required />
-        </label>
-        <br />
-        <label>
-          Description:
-          <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required />
-        </label>
-        <br />
-        <button type="submit">Add Room</button>
-      </form>
-    </div>
+    <>
+      <Row>
+        <Col lg={7} sm={12}>
+          <Card bg="light" text="dark" className="mb-2 shadow-bottom-right">
+            <Card.Header className="py-4 px-5">Rooms</Card.Header>
+            <Card.Body
+              style={{
+                padding: "20px 70px 0 50px",
+              }}
+            >
+              <Card.Title className="p-3">Room List</Card.Title>
+              {loading && (
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              )}
+              {!loading && <RoomList rooms={rooms} setRooms={setRooms} />}
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col lg={5} sm={12}>
+          <div className="d-flex flex-column justify-content-center align-items-center">
+            <h2 className="mt-2">Add Room</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>Room Number:</label>
+                <input
+                  type="text"
+                  value={roomNumber}
+                  onChange={(e) => setRoomNumber(e.target.value)}
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label>Capacity:</label>
+                <input
+                  type="number"
+                  value={capacity}
+                  onChange={(e) => setCapacity(e.target.value)}
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label>Type:</label>
+                <input
+                  type="text"
+                  value={type}
+                  onChange={(e) => setType(e.target.value)}
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label>Description:</label>
+                <input
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  required
+                  className="form-control"
+                />
+              </div>
+              <button
+                type="submit"
+                className="d-block mx-auto btn btn-secondary"
+              >
+                Add Room
+              </button>
+            </form>
+            <Modal show={!!modal} onHide={handleClose} variant="secondary">
+              <Modal.Header closeButton>
+                <Modal.Title>Message</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>{modal}</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        </Col>
+      </Row>
+    </>
   );
 };
 
--- client/src/pages/RoomManagementPage.js
+++ client/src/pages/RoomManagementPage.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import RoomList from '../components/RoomList';
-import AddRoomForm from '../components/AddRoomForm';
-import RoomSearch from '../components/RoomSearch';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import React from "react";
+import RoomList from "../components/RoomList";
+import AddRoomForm from "../components/AddRoomForm";
+import RoomSearch from "../components/RoomSearch";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Container } from "react-bootstrap";
 
 export const RoomManagementPage = () => {
-
   return (
-    <div className="room-management">
-      <h1>Room Management</h1>
-      <RoomList />
+    <Container className="room-management">
+      <h1 className="text-center m-2 my-5">Room Management</h1>
       <AddRoomForm />
-      <RoomSearch />
-      <ToastContainer />
-    </div>
+    </Container>
   );
 };
