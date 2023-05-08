36a11907b09ac92f8127bc485b9c130e15c9db1c
mj351 <80540936+mj351@users.noreply.github.com>
2023-05-08 19:40:29 +0100

    update userlist.js and add css to user Managment

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,45 +1,190 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_BASE_URL } from '../config';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../config";
+import { Modal, Table, Button } from "react-bootstrap";
+import { MdOutlineModeEditOutline } from "react-icons/md";
+import { RiDeleteBin2Line } from "react-icons/ri";
+import Select from "react-select";
 
-const UserList = () => {
-  const [users, setUsers] = useState([]);
+const UserList = ({ users, ROLE, setUsers }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [modal, setModal] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [userId, setUserId] = useState("");
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get(`${API_BASE_URL}/api/users`);
-        setUsers(response.data);
-        console.log(response);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching users:', error);
+  const handleClose = () => {
+    setModal(null);
+  };
+  const handleShowEditModal = (user) => {
+    setUserId(user._id);
+    // console.log(user);
+    setUsername(user.username);
+
+    setShowEditModal(true);
+  };
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+    handleCloseEditModal();
+
+    try {
+      // console.log(role);
+      if (!password || !role.value) {
+        setModal("username, password, and role are required");
+        return;
       }
-    };
+      const updatedRoom = { username, password, role: role.value };
+      // // console.log({ updatedRoom });
+      const { data } = await axios.put(
+        `${API_BASE_URL}/api/users/${userId}`,
+        updatedRoom
+      );
+      setUsers(users.map((user) => (user._id === userId ? data : user)));
+      // toast.success("Room updated successfully");
+      setUserId(null);
+      setModal("User updated successfully");
+    } catch (error) {
+      // console.log(error);
+      setModal("Failed to update user");
+    }
+  };
 
-    fetchUsers();
-  }, []);
-
+  const handleDelete = async (userId) => {
+    try {
+      await axios.delete(`${API_BASE_URL}/api/users/${userId}`);
+      setUsers(users.filter((user) => user._id !== userId));
+      // toast.success("Room deleted successfully");
+      setModal("User deleted successfully");
+    } catch (error) {
+      setModal("Failed to delete user");
+    }
+  };
+  const handleCloseEditModal = () => setShowEditModal(false);
   return (
-    <div>
-      <h2>User List</h2>
-      <table>
+    <>
+      <Table bordered={false} className="bg-transparent">
         <thead>
           <tr>
+            <th>#</th>
             <th>UserName</th>
             <th>Role</th>
+            <th>Action</th>
           </tr>
         </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id}>
+        {users.map((user, i) => (
+          <tbody key={user._id}>
+            <tr>
+              <td>{i + 1}</td>
               <td>{user.username}</td>
               <td>{user.role}</td>
+              <td>
+                <MdOutlineModeEditOutline
+                  style={{ marginRight: "5px", cursor: "pointer" }}
+                  onClick={() => handleShowEditModal(user)}
+                />
+                <RiDeleteBin2Line
+                  style={{ marginLeft: "5px", cursor: "pointer" }}
+                  onClick={() => handleDelete(user._id)}
+                />
+              </td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        ))}
+      </Table>
+      <Modal show={!!modal} onHide={handleClose} variant="secondary">
+        <Modal.Header closeButton>
+          <Modal.Title>Message</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modal}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showEditModal} onHide={handleCloseEditModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="form-group">
+              <label>UserName:</label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="form-control"
+              />
+            </div>
+
+            <div className="form-group">
+              <Select
+                id="userId"
+                // className="form-control"
+                value={role}
+                onChange={setRole}
+                options={ROLE}
+                required={true}
+                placeholder={
+                  <div>
+                    <span style={{ marginLeft: "10px", color: "black" }}>
+                      Select Role
+                    </span>
+                  </div>
+                }
+                styles={{
+                  control: (provided) => ({
+                    ...provided,
+                    minHeight: "40px",
+                    borderColor: "#ced4da",
+                    "&:hover": { borderColor: "#80bdff" },
+                  }),
+                  placeholder: (provided) => ({
+                    ...provided,
+                    color: "black",
+                  }),
+                  singleValue: (provided) => ({
+                    ...provided,
+                    color: "black",
+                  }),
+                  option: (provided, state) => ({
+                    ...provided,
+                    backgroundColor: state.isFocused ? "#80bdff" : "white",
+                    color: state.isFocused ? "white" : "black",
+                  }),
+                }}
+                isSearchable={false}
+              />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-secondary" onClick={handleCloseEditModal}>
+            Close
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={handleUpdate}
+            type="submit"
+          >
+            Save Changes
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
