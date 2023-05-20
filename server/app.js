e6d2ad0f38c2384aaaa8f0b4832e6921839cc462
mj351 <80540936+mj351@users.noreply.github.com>
2023-05-20 01:21:40 +0100

    add a new line to app.js file in server folder

--- server/app.js
+++ server/app.js
@@ -41,6 +41,14 @@ app.use('/api/students', studentRoutes);
 app.use('/api/rooms', roomRoutes);
 app.use('/api/bookings', bookingRoutes)
 
+// Error handling middleware
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 // Start the Serve - https://stackoverflow.com/questions/4840879/nodejs-how-to-get-the-servers-port
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`app listening on port ${port}`));
